a8b5adb363faa10e40a73e437fbc3f9d509dfffa
Martin M <mtendotnet@users.noreply.github.com>
2018-09-06 22:59:50 +0100

    build: configure eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+/* eslint react/jsx-filename-extension: "off" */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
