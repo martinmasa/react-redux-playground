b83e79a522bfc37bb77032d5543a6b28321e02ba
Martin M <mtendotnet@users.noreply.github.com>
2018-09-07 20:08:24 +0100

    feat: add styling to HelloApp

--- src/HelloApp/index.jsx
+++ src/HelloApp/index.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { createStore } from 'redux';
+import styled from 'styled-components';
 
 import reducer from './reducer';
 
+const Hello = styled.div`
+  color: #2c3e50;
+  font-size: 3em;
+  position: relative;
+
+  &:after {
+    position: absolute;
+    z-index: -1;
+    content: "";
+    bottom: -10%;
+    left: 0;
+    height: 40%;
+    width: 100%;
+    background: #f0898b;
+  }
+`;
+
+const Tech = styled.span`
+  font-weight: 700;
+  font-style: italic;
+`;
+
 const HelloWorld = ({ tech }) => {
   return (
-    <div>
-      <h1>Hello World {tech}</h1>
-    </div>
+    <Hello>
+      Hello World <Tech>{tech}!</Tech>
+    </Hello>
   );
 };
 
