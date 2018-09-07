6c4d67ffff465a390be2e70cd9b63236bec57157
Martin M <mtendotnet@users.noreply.github.com>
2018-09-07 20:55:43 +0100

    feat: add actions and reducer logic to update store state

--- src/HelloApp/index.jsx
+++ src/HelloApp/index.jsx
@@ -2,7 +2,59 @@ import React from 'react';
 import { createStore } from 'redux';
 import styled from 'styled-components';
 
-import reducer from './reducer';
+import reducer, { SET_TECHNOLOGY } from './reducer';
+
+const techList = [
+  { key: 'javascript', value: 'JavaScript' },
+  { key: 'react', value: 'React' },
+  { key: 'redux', value: 'Redux' }
+];
+
+const HelloWorld = ({ tech }) => {
+  return (
+    <Hello>
+      Hello World <Tech>{tech}!</Tech>
+    </Hello>
+  );
+};
+
+class HelloApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.store = createStore(reducer);
+    this.setTechnology = this.setTechnology.bind(this);
+  }
+
+  componentDidMount() {
+    this.store.subscribe(this.forceUpdate.bind(this));
+  }
+
+  setTechnology(newTech) {
+    this.store.dispatch({
+      type: SET_TECHNOLOGY,
+      tech: newTech
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <HelloWorld tech={this.store.getState().tech} />
+        {techList.map(({ key, value }) => (
+          <Button
+            key={key}
+            onClick={() => this.setTechnology(value)}
+          >
+            {value}
+          </Button>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default HelloApp;
 
 const Hello = styled.div`
   color: #2c3e50;
@@ -26,26 +78,20 @@ const Tech = styled.span`
   font-style: italic;
 `;
 
-const HelloWorld = ({ tech }) => {
-  return (
-    <Hello>
-      Hello World <Tech>{tech}!</Tech>
-    </Hello>
-  );
-};
+const Button = styled.button`
+  outline: 0;
+  background: #6059FF;
+  color: #fff;
+  font-size: 1rem;
+  padding: 1rem;
+  margin: 1rem;
+  border: 0;
+  box-shadow: none;
+  font-weight: 400;
+  cursor: pointer;
+  border-radius: 10px;
 
-class HelloApp extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.store = createStore(reducer);
+  &:hover {
+    opacity: 0.95;
   }
-
-  render() {
-    return (
-      <HelloWorld tech={this.store.getState().tech} />
-    );
-  }
-}
-
-export default HelloApp;
+`;
--- src/HelloApp/reducer.js
+++ src/HelloApp/reducer.js
@@ -1,10 +1,20 @@
 
+export const SET_TECHNOLOGY = 'hello/SET_TECHNOLOGY';
+
 const initialState = {
   tech: 'React'
 };
 
-const reducer = (state = initialState) => {
-  return state;
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_TECHNOLOGY:
+      return {
+        ...state,
+        tech: action.tech
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
