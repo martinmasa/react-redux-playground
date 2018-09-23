3199d943a80195aaa3695b5108037e5342794ca0
Martin M <mtendotnet@users.noreply.github.com>
2018-09-23 23:18:44 +0100

    build: add redux dev tools config to the store

--- src/SkypeyApp/store/index.js
+++ src/SkypeyApp/store/index.js
@@ -5,7 +5,8 @@ import { contacts } from '../data';
 
 const store = createStore(
   reducer,
-  { contacts }
+  { contacts },
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // eslint-disable-line
 );
 
 export default store;
